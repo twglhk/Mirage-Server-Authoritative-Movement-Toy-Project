5c5e6908e9e6fc225149695306e7900533aa6d18
John <twglhk@gmail.com>
2021-09-07 09:35:46 +0900

    update : attach Mirage to FixedUpdateManager, Motor

--- Assets/Scripts/Motor.cs
+++ Assets/Scripts/Motor.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WardGames.John.AuthoritativeMovement.Manager;
+using Mirage;
 
-public class Motor : MonoBehaviour
+namespace WardGames.John.AuthoritativeMovement.Motors
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Motor : NetworkBehaviour
     {
-        
-    }
+        #region Private
+        /// <summary>
+        /// Rigidbody on this object
+        /// </summary>
+        private Rigidbody _rigidbody;
+        #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            FixedUpdateManager.OnFixedUpdate += FixedUpdateManager_OnFixedUpdate;
+            Identity.OnStartServer.AddListener(OnStartServer);
+        }
+
+        private void OnStartServer()
+        {
+
+        }
+
+        /// <summary>
+        /// Initialize this script for use. Should only be called once.
+        /// </summary>
+        private void NetworkFirstInitialize()
+        {
+
+        }
+
+        private void FixedUpdateManager_OnFixedUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
