c7295201b727a0c32b2f0109171d008b4687649e
John <twglhk@gmail.com>
2021-09-23 09:35:24 +0900

    add : Mirage NetworkRigidbodySmoother

---
+++ Assets/Scripts/NetworkRigidbodySmoother.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirage;
+
+namespace WardGames.John.AuthoritativeMovement.Motors
+{
+    public class NetworkRigidbodySmoother : NetworkBehaviour
+    {
+        
+    }
+}
