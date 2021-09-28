119d319993b769c655dbdcfa65ba0b6a264c70f7
John <twglhk@gmail.com>
2021-09-28 09:44:57 +0900

    update : smooth rigidbody for client

--- Assets/Scripts/NetworkRigidbodySmoother.cs
+++ Assets/Scripts/NetworkRigidbodySmoother.cs
@@ -13,6 +13,33 @@ namespace WardGames.John.AuthoritativeMovement.Motors
         /// True if subscribed to FixedUpdateManager events.
         /// </summary>
         private bool _subscribed = false;
+        /// <summary>
+        /// Position before simulation is performed.
+        /// </summary>
+        private Vector3 _prePosition;
+        /// <summary>
+        /// Rotation before simulation is performed.
+        /// </summary>
+        private Quaternion _preRotation;
+        /// <summary>
+        /// Position after simulation is performed.
+        /// </summary>
+        private Vector3 _postPosition;
+        /// <summary>
+        /// Rotation after simulation is performed.
+        /// </summary>
+        private Quaternion _postRotation;
+        /// <summary>
+        /// Time passed since last fixed frame.
+        /// </summary>
+        private float _frameTimePassed = 0f;
+        #endregion
+
+        #region Const.
+        /// <summary>
+        /// Multiplier to apply towards delta time.
+        /// </summary>
+        private const float FRAME_TIME_MULTIPLIER = 0.75f;
         #endregion
 
         private void Awake()
@@ -22,12 +49,39 @@ namespace WardGames.John.AuthoritativeMovement.Motors
 
         private void OnAuthorityChanged(bool hasAuthority)
         {
+            if(hasAuthority)
+                SubscribeToFixedUpdateManager(true);
+            else
+                SubscribeToFixedUpdateManager(false);
+        }
 
+        private void OnEnable()
+        {
+            if (HasAuthority)
+                SubscribeToFixedUpdateManager(true);
         }
 
         private void OnDisable()
         {
-            
+            SubscribeToFixedUpdateManager(false);
+        }
+
+        private void Update()
+        {
+            if (HasAuthority)
+                Smooth();
+        }
+
+        /// <summary>
+        /// Smooths position and rotation to zero values.
+        /// </summary>
+        private void Smooth()
+        {
+            _frameTimePassed += (Time.deltaTime * FRAME_TIME_MULTIPLIER);
+            float percent = Mathf.InverseLerp(0f, FixedUpdateManager.AdjustedFixedDeltaTime, _frameTimePassed);
+
+            transform.position = Vector3.Lerp(_prePosition, transform.position, percent);
+            transform.rotation = Quaternion.Lerp(_preRotation, transform.rotation, percent);
         }
 
         /// <summary>
@@ -53,12 +107,18 @@ namespace WardGames.John.AuthoritativeMovement.Motors
 
         private void FixedUpdateManager_OnPostFixedUpdate()
         {
-            
+            _frameTimePassed = 0f;
+            transform.position = _prePosition;
+            transform.rotation = _preRotation;
+
+            _postPosition = transform.position;
+            _postRotation = transform.rotation;
         }
 
         private void FixedUpdateManager_OnPreFixedUpdate()
         {
-            
+            _prePosition = transform.position;
+            _preRotation = transform.rotation;
         }
     }
 }
